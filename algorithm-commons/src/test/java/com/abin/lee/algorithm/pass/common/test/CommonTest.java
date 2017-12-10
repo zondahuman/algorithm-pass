a17da3f3b34e85e78daa8dec3bdc70b1dee74460
zondahuman <zondahuman@gmail.com>
2017-12-10 16:49:44 +0800

    add

---
+++ algorithm-commons/src/test/java/com/abin/lee/algorithm/pass/common/test/CommonTest.java
@@ -0,0 +1,9 @@
+package com.abin.lee.algorithm.pass.common.test;
+
+/**
+ * Created by abin on 2017/12/10 2017/12/10.
+ * algorithm-pass
+ * com.abin.lee.algorithm.pass.common.test
+ */
+public class CommonTest {
+}
---
+++ algorithm-lintcode/src/test/java/com/abin/lee/algorithm/pass/lintcode/test/LintCodeTest.java
@@ -0,0 +1,12 @@
+package com.abin.lee.algorithm.pass.lintcode.test;
+
+/**
+ * Created by abin on 2017/12/10 2017/12/10.
+ * algorithm-pass
+ * com.abin.lee.algorithm.pass.lintcode.test
+ */
+public class LintCodeTest {
+
+
+
+}
